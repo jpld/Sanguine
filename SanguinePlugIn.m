e4a5aba076698327b9bd58e498002b23058604fc
jpld <imjunpyo@gmail.com>
2009-12-14 16:23:28 -0800

    reformat braces and return type, also workaround bad method naming

--- SanguinePlugIn.m
+++ SanguinePlugIn.m
@@ -6,13 +6,17 @@
 //  Copyright (c) 2009 __MyCompanyName__. All rights reserved.
 //
 
-/* It's highly recommended to use CGL macros instead of changing the current context for plug-ins that perform OpenGL rendering */
-#import <OpenGL/CGLMacro.h>
-
 #import "SanguinePlugIn.h"
 
 #define	kQCPlugIn_Name				@"Sanguine"
 #define	kQCPlugIn_Description		@"Sanguine description"
+
+
+// WORKAROUND - naming violation for cocoa memory management
+@interface QCPlugIn(CameraObscuraAdditions)
+- (QCPlugInViewController*)createViewController NS_RETURNS_RETAINED;
+@end
+
 
 @implementation SanguinePlugIn
 
@@ -21,8 +25,7 @@ Here you need to declare the input / output properties as dynamic as Quartz Comp
 @dynamic inputFoo, outputBar;
 */
 
-+ (NSDictionary*) attributes
-{
++ (NSDictionary*)attributes {
 	/*
 	Return a dictionary of attributes describing the plug-in (QCPlugInAttributeNameKey, QCPlugInAttributeDescriptionKey...).
 	*/
@@ -30,8 +33,7 @@ Here you need to declare the input / output properties as dynamic as Quartz Comp
 	return [NSDictionary dictionaryWithObjectsAndKeys:kQCPlugIn_Name, QCPlugInAttributeNameKey, kQCPlugIn_Description, QCPlugInAttributeDescriptionKey, nil];
 }
 
-+ (NSDictionary*) attributesForPropertyPortWithKey:(NSString*)key
-{
++ (NSDictionary*)attributesForPropertyPortWithKey:(NSString*)key {
 	/*
 	Specify the optional attributes for property based ports (QCPortAttributeNameKey, QCPortAttributeDefaultValueKey...).
 	*/
@@ -39,8 +41,7 @@ Here you need to declare the input / output properties as dynamic as Quartz Comp
 	return nil;
 }
 
-+ (QCPlugInExecutionMode) executionMode
-{
++ (QCPlugInExecutionMode)executionMode {
 	/*
 	Return the execution mode of the plug-in: kQCPlugInExecutionModeProvider, kQCPlugInExecutionModeProcessor, or kQCPlugInExecutionModeConsumer.
 	*/
@@ -48,8 +49,7 @@ Here you need to declare the input / output properties as dynamic as Quartz Comp
 	return kQCPlugInExecutionModeProcessor;
 }
 
-+ (QCPlugInTimeMode) timeMode
-{
++ (QCPlugInTimeMode)timeMode {
 	/*
 	Return the time dependency mode of the plug-in: kQCPlugInTimeModeNone, kQCPlugInTimeModeIdle or kQCPlugInTimeModeTimeBase.
 	*/
@@ -57,7 +57,17 @@ Here you need to declare the input / output properties as dynamic as Quartz Comp
 	return kQCPlugInTimeModeNone;
 }
 
-- (id) init {
++ (NSArray*)plugInKeys {
+	/*
+	Return a list of the KVC keys corresponding to the internal settings of the plug-in.
+	*/
+	
+	return nil;
+}
+
+#pragma mark -
+
+- (id)init {
     self = [super init];
 	if(self) {
 		/*
@@ -68,8 +78,7 @@ Here you need to declare the input / output properties as dynamic as Quartz Comp
 	return self;
 }
 
-- (void) finalize
-{
+- (void)finalize {
 	/*
 	Release any non garbage collected resources created in -init.
 	*/
@@ -77,8 +86,7 @@ Here you need to declare the input / output properties as dynamic as Quartz Comp
 	[super finalize];
 }
 
-- (void) dealloc
-{
+- (void)dealloc {
 	/*
 	Release any resources created in -init.
 	*/
@@ -86,17 +94,9 @@ Here you need to declare the input / output properties as dynamic as Quartz Comp
 	[super dealloc];
 }
 
-+ (NSArray*) plugInKeys
-{
-	/*
-	Return a list of the KVC keys corresponding to the internal settings of the plug-in.
-	*/
-	
-	return nil;
-}
+#pragma mark -
 
-- (id) serializedValueForKey:(NSString*)key;
-{
+- (id)serializedValueForKey:(NSString*)key {
 	/*
 	Provide custom serialization for the plug-in internal settings that are not values complying to the <NSCoding> protocol.
 	The return object must be nil or a PList compatible i.e. NSString, NSNumber, NSDate, NSData, NSArray or NSDictionary.
@@ -105,8 +105,7 @@ Here you need to declare the input / output properties as dynamic as Quartz Comp
 	return [super serializedValueForKey:key];
 }
 
-- (void) setSerializedValue:(id)serializedValue forKey:(NSString*)key
-{
+- (void)setSerializedValue:(id)serializedValue forKey:(NSString*)key {
 	/*
 	Provide deserialization for the plug-in internal settings that were custom serialized in -serializedValueForKey.
 	Deserialize the value, then call [self setValue:value forKey:key] to set the corresponding internal setting of the plug-in instance to that deserialized value.
@@ -115,8 +114,7 @@ Here you need to declare the input / output properties as dynamic as Quartz Comp
 	[super setSerializedValue:serializedValue forKey:key];
 }
 
-- (QCPlugInViewController*) createViewController
-{
+- (QCPlugInViewController*)createViewController {
 	/*
 	Return a new QCPlugInViewController to edit the internal settings of this plug-in instance.
 	You can return a subclass of QCPlugInViewController if necessary.
@@ -125,12 +123,9 @@ Here you need to declare the input / output properties as dynamic as Quartz Comp
 	return [[QCPlugInViewController alloc] initWithPlugIn:self viewNibName:@"Settings"];
 }
 
-@end
+#pragma mark -
 
-@implementation SanguinePlugIn (Execution)
-
-- (BOOL) startExecution:(id<QCPlugInContext>)context
-{
+- (BOOL)startExecution:(id<QCPlugInContext>)context {
 	/*
 	Called by Quartz Composer when rendering of the composition starts: perform any required setup for the plug-in.
 	Return NO in case of fatal failure (this will prevent rendering of the composition to start).
@@ -139,15 +134,13 @@ Here you need to declare the input / output properties as dynamic as Quartz Comp
 	return YES;
 }
 
-- (void) enableExecution:(id<QCPlugInContext>)context
-{
+- (void)enableExecution:(id<QCPlugInContext>)context {
 	/*
 	Called by Quartz Composer when the plug-in instance starts being used by Quartz Composer.
 	*/
 }
 
-- (BOOL) execute:(id<QCPlugInContext>)context atTime:(NSTimeInterval)time withArguments:(NSDictionary*)arguments
-{
+- (BOOL)execute:(id<QCPlugInContext>)context atTime:(NSTimeInterval)time withArguments:(NSDictionary*)arguments {
 	/*
 	Called by Quartz Composer whenever the plug-in instance needs to execute.
 	Only read from the plug-in inputs and produce a result (by writing to the plug-in outputs or rendering to the destination OpenGL context) within that method and nowhere else.
@@ -160,15 +153,13 @@ Here you need to declare the input / output properties as dynamic as Quartz Comp
 	return YES;
 }
 
-- (void) disableExecution:(id<QCPlugInContext>)context
-{
+- (void)disableExecution:(id<QCPlugInContext>)context {
 	/*
 	Called by Quartz Composer when the plug-in instance stops being used by Quartz Composer.
 	*/
 }
 
-- (void) stopExecution:(id<QCPlugInContext>)context
-{
+- (void)stopExecution:(id<QCPlugInContext>)context {
 	/*
 	Called by Quartz Composer when rendering of the composition stops: perform any required cleanup for the plug-in.
 	*/
