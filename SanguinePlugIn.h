88c938826314730d6bb044a80d1bd00ca9399086
jpld <imjunpyo@gmail.com>
2009-12-14 16:16:20 -0800

    initial import of skeleton project

---
+++ SanguinePlugIn.h
@@ -0,0 +1,22 @@
+//
+//  SanguinePlugIn.h
+//  Sanguine
+//
+//  Created by jpld on 14 Dec 2009.
+//  Copyright (c) 2009 __MyCompanyName__. All rights reserved.
+//
+
+#import <Quartz/Quartz.h>
+
+@interface SanguinePlugIn : QCPlugIn
+{
+}
+
+/*
+Declare here the Obj-C 2.0 properties to be used as input and output ports for the plug-in e.g.
+@property double inputFoo;
+@property(assign) NSString* outputBar;
+You can access their values in the appropriate plug-in methods using self.inputFoo or self.inputBar
+*/
+
+@end
