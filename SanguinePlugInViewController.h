860b641cad39b68e3f10f4d6fd1abf76a4ea55ea
jpld <imjunpyo@gmail.com>
2009-12-20 23:50:40 -0800

    add empty view controller to project

---
+++ SanguinePlugInViewController.h
@@ -0,0 +1,12 @@
+//
+//  SanguinePlugInViewController.h
+//  Sanguine
+//
+//  Created by jpld on 20 Dec 2009.
+//  Copyright 2009 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface SanguinePlugInViewController : QCPlugInViewController {}
+@end
---
+++ SanguinePlugInViewController.m
@@ -0,0 +1,12 @@
+//
+//  SanguinePlugInViewController
+//  Sanguine
+//
+//  Created by jpld on 20 Dec 2009.
+//  Copyright 2009 __MyCompanyName__. All rights reserved.
+//
+
+#import "SanguinePlugInViewController.h"
+
+@implementation SanguinePlugInViewController
+@end
